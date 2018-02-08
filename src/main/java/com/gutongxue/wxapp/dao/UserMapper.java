08cbcdd3a1fea9ec978327fea7d84a382e616436
谷若琪 <3003277@qq.com>
2018-02-08 16:53:47 +0800

    谷若琪--修复BUG

--- src/main/java/com/gutongxue/wxapp/dao/UserMapper.java
+++ src/main/java/com/gutongxue/wxapp/dao/UserMapper.java
@@ -11,19 +11,6 @@ import java.util.List;
 @Mapper
 public interface UserMapper {
 
-    @Results(id = "userDO" , value = {
-            @Result(property = "openid" , column = "openid"),
-            @Result(property = "createTime" , column = "gmt_creata"),
-            @Result(property = "loginTime" , column = "gmt_login"),
-            @Result(property = "nickname" , column = "nickname"),
-            @Result(property = "avatar" , column = "avatar"),
-            @Result(property = "ip" , column = "ip"),
-            @Result(property = "country" , column = "country"),
-            @Result(property = "region" , column = "region"),
-            @Result(property = "city" , column = "city"),
-            @Result(property = "isp" , column = "isp")
-    })
-
     @Insert("insert into `gtx_user_info` (`openid`,`gmt_create`,`gmt_login`,`nickname`,`avatar`,`ip`,`country`,`region`,`city`,`isp`)\n" +
             " values(#{user.openid},#{user.createTime},#{user.loginTime},#{user.nickname},#{user.avatar},#{user.ip},#{user.country},#{user.region},#{user.city},#{user.isp})")
     void insertUser(@Param("user")UserDO userDO);
@@ -43,13 +30,25 @@ public interface UserMapper {
             "  where openid = #{user.openid}")
     void updateUser(@Param("user") UserDO userDO);
 
-    @ResultMap("userDO")
+    @Results(id = "userDO" , value = {
+            @Result(property = "openid" , column = "openid"),
+            @Result(property = "createTime" , column = "gmt_creata"),
+            @Result(property = "loginTime" , column = "gmt_login"),
+            @Result(property = "nickname" , column = "nickname"),
+            @Result(property = "avatar" , column = "avatar"),
+            @Result(property = "ip" , column = "ip"),
+            @Result(property = "country" , column = "country"),
+            @Result(property = "region" , column = "region"),
+            @Result(property = "city" , column = "city"),
+            @Result(property = "isp" , column = "isp")
+    })
     @Select("select * from gtx_user_info order by gmt_login desc")
     List<UserDO> listUser();
 
     @Select("select count(*) from gtx_user_info")
     int countUser();
 
+    @ResultMap("userDO")
     @Select("select * from gtx_user_info where openid = #{0}")
     UserDO getUser(String openid);
 
